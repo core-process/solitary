661058a086c7de04b4d13c71b2afac25728db490
Niklas Salmoukas <niklas@salmoukas.com>
2017-01-17 11:39:54 +0100

    transfer properties not values

--- src/module.js
+++ src/module.js
@@ -5,17 +5,42 @@ export default function solitary(privImpl) {
 
   return function internal(pubObj) {
 
+    // check if we have privObj in map already
     if(!privMap.has(pubObj)) {
 
-      const privObj = Object.create(pubObj);
-      for(let prop of Object.keys(privImpl)) {
-        privObj[prop] = privImpl[prop];
+      // get property names
+      const properties = Object.getOwnPropertyNames(pubObj);
+      if(Object.getOwnPropertySymbols) {
+        properties.push(...Object.getOwnPropertySymbols(pubObj));
       }
 
+      // get property descriptors
+      const descriptors = properties.reduce(
+        (result, descriptor) => {
+          result[descriptor] = Object.getOwnPropertyDescriptor(pubObj, descriptor);
+          return result;
+        },
+        { }
+      );
+
+      // create privObj with pubObj as prototype
+      const privObj = Object.create(pubObj);
+
+      // apply properties to privObj
+      for(let property of properties) {
+        Object.defineProperty(
+          privObj,
+          property,
+          descriptors[property]
+        )
+      }
+
+      // store privObj
       privMap.set(pubObj, privObj);
       return privObj;
     }
 
+    // get privObj from map
     return privMap.get(pubObj);
   };
 };
